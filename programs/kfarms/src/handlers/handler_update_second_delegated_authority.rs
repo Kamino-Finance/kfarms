72662488b36f7971385e40fcb6c7ea4b8e67ff8d
Silviu Troscot <silviu.troscot10@gmail.com>
2025-05-12 12:09:51 +0300

    update file

---
+++ programs/kfarms/src/handlers/handler_update_second_delegated_authority.rs
@@ -0,0 +1,39 @@
+use crate::utils::constraints::check_remaining_accounts;
+use crate::FarmState;
+use crate::GlobalConfig;
+use anchor_lang::prelude::*;
+
+pub fn process(ctx: Context<UpdateSecondDelegatedAuthority>) -> Result<()> {
+    check_remaining_accounts(&ctx)?;
+
+    let mut farm_state = ctx.accounts.farm_state.load_mut()?;
+    msg!(
+        "prv second_delegated_authority: {}",
+        farm_state.second_delegated_authority
+    );
+    msg!(
+        "new second_delegated_authority: {}",
+        ctx.accounts.new_second_delegated_authority.key()
+    );
+    farm_state.second_delegated_authority = ctx.accounts.new_second_delegated_authority.key();
+
+    Ok(())
+}
+
+#[derive(Accounts)]
+pub struct UpdateSecondDelegatedAuthority<'info> {
+    #[account(mut)]
+    pub global_admin: Signer<'info>,
+
+    #[account(mut,
+        has_one = global_config,
+    )]
+    pub farm_state: AccountLoader<'info, FarmState>,
+
+    #[account(
+        has_one = global_admin,
+    )]
+    pub global_config: AccountLoader<'info, GlobalConfig>,
+
+    pub new_second_delegated_authority: AccountInfo<'info>,
+}
