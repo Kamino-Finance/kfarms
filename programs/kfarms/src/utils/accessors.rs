204d6d073f1537d13f04be0dd661361db97b6c67
Olivier Eblé <olivier@eble.fr>
2025-07-25 15:30:31 +0200

    Prepare Release 1.6.1 (#15)
    - Allow transfer ownership to use an existing user state as target.

--- programs/kfarms/src/utils/accessors.rs
+++ programs/kfarms/src/utils/accessors.rs
@@ -1,9 +1,17 @@
 use anchor_lang::prelude::AccountInfo;
-use anchor_lang::Result;
+use anchor_lang::{Result, ToAccountInfo};
 
 pub fn mint_decimals(account: &AccountInfo) -> Result<u8> {
     let bytes = account.try_borrow_data()?;
     let mut amount_bytes = [0u8; 1];
     amount_bytes.copy_from_slice(&bytes[0x2C..0x2D]);
     Ok(u8::from_le_bytes(amount_bytes))
+}
+
+pub fn account_discriminator(account: &dyn ToAccountInfo) -> Result<[u8; 8]> {
+    let account = account.to_account_info();
+    let data = account.try_borrow_data()?;
+    let mut disc_bytes = [0u8; 8];
+    disc_bytes.copy_from_slice(&data[..8]);
+    Ok(disc_bytes)
 }
