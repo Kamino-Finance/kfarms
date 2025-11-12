3315d44b7c9c666949bfedcc3b64d15a0a1fa941
Mihai Tataru <mihalex98@gmail.com>
2025-11-12 12:28:45 +0000

    Release 1.6.5

--- programs/kfarms/src/handlers/handler_reward_user_once.rs
+++ programs/kfarms/src/handlers/handler_reward_user_once.rs
@@ -5,7 +5,17 @@ use crate::{
     FarmState,
 };
 
-pub fn process(ctx: Context<RewardUserOnce>, reward_index: u64, amount: u64) -> Result<()> {
+
+
+
+
+
+pub fn process(
+    ctx: Context<RewardUserOnce>,
+    reward_index: u64,
+    amount: u64,
+    expected_reward_issued_unclaimed: u64,
+) -> Result<()> {
     check_remaining_accounts(&ctx)?;
 
     let mut farm_state = ctx.accounts.farm_state.load_mut()?;
@@ -15,6 +25,12 @@ pub fn process(ctx: Context<RewardUserOnce>, reward_index: u64, amount: u64) -> 
         farm_state.is_reward_user_once_enabled,
         1,
         FarmError::RewardUserOnceFeatureDisabled
+    );
+
+    require_eq!(
+        user_state.rewards_issued_unclaimed[reward_index as usize],
+        expected_reward_issued_unclaimed,
+        FarmError::CurrentRewardIssuedUnclaimedMismatch
     );
 
     farm_operations::reward_user_once(&mut farm_state, &mut user_state, reward_index, amount)?;
